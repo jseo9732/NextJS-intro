91709a9b7f2b618d50232049eb785b8677eae3ec
jisu Seo <jseo9732@gmail.com>
2023-02-04 02:07:08 +0900

    #2.0 Patterns

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,9 @@
-import NavBar from '../components/NavBar';
-// 다른 컴포넌트에서는 globals.css import가 불가능 하지만 _app 파일에서는 가능하다.
-// import "../styles/globals.css"
+import Layout from '@/components/Layout';
 
 export default function App({ Component, pageProps }: any) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-      <style jsx global>{`
-        a {
-          color: white;
-        }
-      `}</style>
-    </>
+    </Layout>
   );
 }
