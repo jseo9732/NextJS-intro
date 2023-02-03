81ab298de955a56102338d8a84933380ef7199a7
jisu Seo <jseo9732@gmail.com>
2023-02-03 23:20:57 +0900

    #1.4 CSS Modules - 3

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,10 +6,19 @@ export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link href="/" className={router.pathname === '/' ? styles.active : ''}>
+      {/* 스타일 클래스 네임이 여러개인 경우 아래와 같이 입력해도 동작하지 않는다. => 특정 문자열을 만들어서 삽입해야한다. */}
+      {/* <Link href="/" className={styles.link, router.pathname === '/' ? styles.active : ''}> */}
+
+      {/* 방법 1 */}
+      <Link href="/" className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`}>
         Home
       </Link>
-      <Link href="/about" className={router.pathname === '/about' ? styles.active : ''}>
+
+      {/* 방법 2 */}
+      <Link
+        href="/about"
+        className={[styles.link, router.pathname === '/about' ? styles.active : ''].join(' ')}
+      >
         About
       </Link>
     </nav>
