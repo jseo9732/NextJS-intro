861bec3976e9f08742e120f527f4f4347e37393d
jisu Seo <jseo9732@gmail.com>
2023-02-04 00:58:49 +0900

    #1.6 Custom App - 3__app파일 커스텀

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
+import NavBar from '../components/NavBar';
+// 다른 컴포넌트에서는 globals.css import가 불가능 하지만 _app 파일에서는 가능하다.
+// import "../styles/globals.css"
+
 export default function App({ Component, pageProps }: any) {
   return (
-    <div>
+    <>
+      <NavBar />
       <Component {...pageProps} />
-      <span>hello</span>
-    </div>
+      <style jsx global>{`
+        a {
+          color: white;
+        }
+      `}</style>
+    </>
   );
 }
