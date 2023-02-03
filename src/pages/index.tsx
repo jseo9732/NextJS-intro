959c442148cbb18a84708bb9d684a7672c26b8e8
jisu Seo <jseo9732@gmail.com>
2023-02-04 04:16:47 +0900

    #2.4 Recap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home({ results }: InferGetServerSidePropsType<GetServerS
       <Seo title="Home" />
       {results?.map((movie: IMovie) => (
         <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
@@ -22,6 +22,9 @@ export default function Home({ results }: InferGetServerSidePropsType<GetServerS
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
