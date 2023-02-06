c72a191e36566fcfae576960e6811b59d0088500
jisu Seo <jseo9732@gmail.com>
2023-02-06 17:52:31 +0900

    #2.7 Catch All -  server side rendering

--- src/pages/movies/[...params].tsx
+++ src/pages/movies/[...params].tsx
@@ -1,12 +1,26 @@
+import Seo from '@/components/Seo';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { useRouter } from 'next/router';
 
-export default function Detail() {
+export default function Detail({ params }: InferGetServerSidePropsType<GetServerSideProps>) {
   const router = useRouter();
-  const [title, id] = router.query.params as string[];
-  console.log(router);
+  const [title, id] = params as string[];
   return (
     <div>
+      <Seo title={title} />
       <h4>{title}</h4>
     </div>
   );
+}
+
+interface Iparams {
+  params: {
+    params: GetServerSideProps;
+  };
+}
+
+export async function getServerSideProps({ params: { params } }: Iparams) {
+  return {
+    props: { params },
+  };
 }
